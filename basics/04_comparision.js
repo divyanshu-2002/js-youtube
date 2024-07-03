15e859073f6d8ca68569fbc1cf8f85bed62ec462
divyanshu-2002 <y22271012@dhsgsu.edu.in>
2024-07-03 14:47:11 +0000

    comparisionOperators

---
+++ basics/04_comparision.js
@@ -0,0 +1,15 @@
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+// console.log(null <=0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+
+// We always avoid using the above code conditions 
+// we always write clean code
+
+// === strict check (it checks data types along with value)
+
