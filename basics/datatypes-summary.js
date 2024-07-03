6a6b544af4e8cb831c71386073ce2f3647aacec9
divyanshu-2002 <y22271012@dhsgsu.edu.in>
2024-07-03 14:47:45 +0000

    datatype-Summary

---
+++ basics/datatypes-summary.js
@@ -0,0 +1,34 @@
+// Primitive
+
+const score = 100
+const scoreValue = 100.33
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId)
+
+//const bigNumber = 23244555525246488n
+
+
+//Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"]  // array
+
+let myObj = {             //objects
+    name: "divyanshu",
+    age: 24,
+}
+
+const myFunction = function(){   // function
+    console.log("Hello,World");
+}
+
+
+console.log(typeof scoreValue)
+
+
